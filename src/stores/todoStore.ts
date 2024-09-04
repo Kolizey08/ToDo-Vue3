2c07b4c6156bd2e9ee42a5af1494a0758abdd00d
Kolizey_08 <magamedbagiev@gmail.com>
2024-09-04 16:22:58 +0300

    попробовал оба варианта: options api, compasition api; и реолизовал возможность сохронение данных в localstorage

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -1,5 +1,54 @@
+// import { defineStore } from 'pinia'
+
+// export interface Tasks {
+//   title: string
+//   id: number
+//   text: string
+//   priority: 'низкий' | 'средний' | 'высокий'
+//   completed: boolean
+// }
+
+// export const useTodoStore = defineStore('todoStore', {
+//   state: () => ({
+//     tasks: [] as Tasks[]
+//   }),
+
+//   actions: {
+//     addTask(title: string, text: string, priority: 'низкий' | 'средний' | 'высокий'): void {
+//       this.tasks.push({
+//         title,
+//         id: Date.now(),
+//         text,
+//         priority,
+//         completed: false
+//       })
+//     },
+
+//     updateTask(updatedTask: Tasks): void {
+//       const index = this.tasks.findIndex((task) => task.id === updatedTask.id)
+//       if (index !== -1) {
+//         this.tasks[index] = updatedTask
+//       }
+//     },
+
+//     deleteTask(id: number): void {
+//       const index = this.tasks.findIndex((task) => task.id === id)
+//       if (index !== -1) {
+//         this.tasks.splice(index, 1)
+//       }
+//     },
+
+//     updateStateTask(id: number): void {
+//       const task = this.tasks.find((el) => el.id === id)
+//       if (task) {
+//         task.completed = !task.completed
+//       }
+//     }
+//   }
+// })
+
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 export interface Tasks {
   title: string
@@ -11,6 +60,12 @@ export interface Tasks {
 
 export const useTodoStore = defineStore('todoStore', () => {
   const tasks = ref<Tasks[]>([])
+
+  const tasksLocalstorage = localStorage.getItem('tasks')
+  if (tasksLocalstorage) {
+    tasks.value = JSON.parse(tasksLocalstorage)._value
+    console.log(JSON.parse(tasksLocalstorage))
+  }
 
   function addTask(title: string, text: string, priority: 'низкий' | 'средний' | 'высокий'): void {
     tasks.value.push({
@@ -42,6 +97,10 @@ export const useTodoStore = defineStore('todoStore', () => {
       task.completed = !task.completed
     }
   }
+
+  watch(() => tasks, (state) => {
+    localStorage.setItem('tasks', JSON.stringify(state))
+  }, {deep: true})
 
   return { tasks, addTask, updateTask, deleteTask, updateStateTask }
 })
